17c86956b436d616125e192a86169b3297d3d9bd
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-03-19 18:10:24 +0000

    4r2r

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function enterClicked() {
+  // input
+  const firstName = parseInt(document.getElementById("number").value)
+  const userAge = document.getElementById("name").value
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your address is: " + firstName + " " + userAge + "."
+}
